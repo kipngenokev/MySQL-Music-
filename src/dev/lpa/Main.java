b20c7bdd110a4c7520c14148c0d6239d9f5054b7
brian070069 <kkipngeno409@gmail.com>
2024-10-06 14:11:49 +0300

    connecting my Music database with jdbc

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        String username= JOptionPane.showInputDialog(null,"Enter DB username");
+    }
+}
